b54998d97a06114297798f9490228f49f6839a63
Anders Spear <anders@aspear.net>
2022-10-02 00:11:51 -0400

    finally fixed paths

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,26 +1,15 @@
 #!/home/pi/n/bin/node
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-<<<<<<< HEAD
-const { clientID, guildID, token } = require('config.json');
-=======
 const { clientID, guildID, token } = require('./config.json');
->>>>>>> fb2cd97 (made paths relative)
 const fs = require("node:fs");
 
 const commands = [];
 
-<<<<<<< HEAD
-const commandFiles = fs.readdirSync('commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`commands/${file}`);
-=======
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
->>>>>>> fb2cd97 (made paths relative)
 	commands.push(command.data.toJSON());
 }
 
